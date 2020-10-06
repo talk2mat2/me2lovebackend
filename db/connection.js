171363105666f9db1d40c5823700ff5fe84e5d22
talk2mat2 <talk2mat2@yahoo.com>
2020-10-06 16:38:02 +0100

    modified somethings

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-  }
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 // var url = "mongodb://localhost:27017/martins"
-const url = `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@cluster0-gussd.mongodb.net/me2love?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${Martins_c}:${chibuzo1}@cluster0-gussd.mongodb.net/me2love?retryWrites=true&w=majority`;
 
 const connectDB = async () => {
-  await mongoose.connect( 
+  await mongoose.connect(
     url,
     { useUnifiedTopology: true, useNewUrlParser: true },
     (err, success) => {
-      if (err) return console.log('error occureed, connction refused',err);
-     
-     
+      if (err) return console.log("error occureed, connction refused", err);
+
       return console.log("connected to remote mongodb server");
     }
   );
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 exports.LoginbyJWT = function(req,res,next){
     const token =req.headers.authorization
-    jwt.verify(token,process.env.JWTKEY,function(err,decodedToken)
+    jwt.verify(token,METUSJONES,function(err,decodedToken)
     {if(err){res.status(401).send({message:'auth failed, login to continue'})}
     
     else{req.body.id=decodedToken.user._id;
