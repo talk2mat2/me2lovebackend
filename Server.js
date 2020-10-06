7774891a8d0a66d00a1da54aa87fbe6ded5ef50d
talk2mat2 <talk2mat2@yahoo.com>
2020-10-06 18:42:21 +0100

    c

--- Server.js
+++ Server.js
@@ -8,15 +8,12 @@ const UserRoutes= require('./routes/userroutes')
 
 process.env.NODE_ENV !== "production" ? require("dotenv").config() : null;
 connectDB();
-const Port = process.env.Port || 8080;
-console.log(process.env.Port)
+const port = process.env.PORT || 5000;
+
 App.use(cors())
 App.use(express.json({extended:false}));
 App.use('/api/v1',UserRoutes)
 App.get('/',(req,res)=>{
     res.status(200).send({message:"welcome to me2love backend servers"})
 })
-App.listen(Port, (err, successs) => {
-  if (err) throw err;
-  console.log(`server running on port ${Port}`);
-});
+app.listen(port,()=>(console.log('server runinng ')));
