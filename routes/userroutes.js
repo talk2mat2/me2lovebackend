77997a3a93abcb91f993e013a30522cee0cf5040
talk2mat2 <talk2mat2@yahoo.com>
2020-10-15 12:27:35 +0100

    updated user model

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -15,11 +15,14 @@ const { Login, Register, UpdateUserData } = require("../controllers/user");
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/image/uploads/profilepics");
+    cb(null, "./public/image");
   },
-  // filename: function (req, file, cb) {
-  //   cb(null, file.fieldname + '-' + req.user.id+ path.extname(file.originalname))
-  // }
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + req.body.id + path.extname(file.originalname)
+    );
+  },
 });
 
 const fileFilter = (req, file, cb) => {
@@ -33,7 +36,12 @@ const fileFilter = (req, file, cb) => {
     cb(null, false);
   }
 };
-const upload = multer({ storage: storage, fileFilter: fileFilter });
+const upload = multer({
+  // dest: "public/image/uploaded",
+  storage: storage,
+
+  fileFilter: fileFilter,
+});
 
 Router.post("/login", Login);
 
@@ -42,54 +50,67 @@ Router.post("/Register", Register);
 Router.post("/Update", LoginbyJWT, UpdateUserData);
 Router.post(
   "/Update/UploadImg",
+
+  upload.single("file"),
   LoginbyJWT,
-  upload.single("profilepicture"),
-  (req, res) => {
+  async (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
     const file = req.file;
+
     const filter = { _id: req.body.id };
-    if (!file) {
-      return res.status(501).send({ error: "no image provided" });
-    }
-    if (file) {
-      cloudinary.uploader.upload(
-        path,
-        { public_id: `image/${req.body.id}/${uniqueFilename}`, tags: `image` }, // directory and tags are optional
-        async function (err, image) {
-          if (err)
-            return res.send({
-              message: "unable to perfom the requested operation",
-            });
-          console.log("file uploaded to Cloudinary server");
-          // console.log(path)
-          // remove file from server
-          const fs = require("fs");
-          fs.unlinkSync(path);
-          // return image details
-          console.log(image.secure_url);
 
-          // const profilepicture= '/image/uploads/profilepics/'+req.file.filename;
+    await UserSchema.findOne(filter)
+      .then((user) => {
+        if (user.Pictures.length < 5) {
+          if (!file) {
+            return res.status(501).send({ message: "no image provided" });
+          }
+          const Path = req.file.path;
+          // const uniqueFilename = req.user.id + req.user.firstname;
 
-          await UserSchema.findByIdAndUpdate(
-            filter,
-            {
-              Pictures: [image.secure_url],
-            },
-            { new: true, useFindAndModify: false }
-          )
-            .then((user) => {
-              return res.json({
-                userdata: user,
-              });
-            })
-            .catch((err) => {
-              console.log(err);
-              res
-                .status(401)
-                .send({ message: "an error occured,unable to upload" });
-            });
+          if (file) {
+            const uniqueFilename = `${req.body.id}me2love`;
+            cloudinary.uploader.upload(
+              Path,
+              {
+                public_id: `image/${req.body.id}/${uniqueFilename}`,
+                tags: `image`,
+              },
+              function (err, image) {
+                if (err) {
+                  console.log(err);
+                  return res.send({
+                    message: "unable to perfom the requested operation",
+                  });
+                }
+                console.log("file uploaded to Cloudinary server");
+
+                // remove file from server
+                const fs = require("fs");
+                fs.unlinkSync(Path);
+                // return image details
+                console.log(image.secure_url);
+
+                user.Pictures.push({ url: image.secure_url });
+                user.save();
+                return res.status(200).send({ userdata: user });
+              }
+            );
+          }
+        } else {
+          return res.status(501).send({
+            message: "maximum picture numbers  of 5reached",
+          });
         }
-      );
-    }
+      })
+      .catch((err) => {
+        console.log(err, "no user found with that user id provided");
+        res.status(501).send({
+          message:
+            "an error occucured, unable to process your request, thats all we know",
+        });
+      });
   }
 );
 
