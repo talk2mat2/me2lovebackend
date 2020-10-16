d302ad3e15833cfc9ff740f9107edbf1a6e8ff6f
talk2mat2 <talk2mat2@yahoo.com>
2020-10-17 00:42:49 +0100

    __

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -22,7 +22,7 @@ const {
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/image/uploads");
+    cb(null, "./public/image");
   },
   filename: function (req, file, cb) {
     cb(
